afb282451aaba7fbd472d08629edddfa42886cc0
Eric Zimmermann <tarnkappe89@gmx.de>
2014-04-26 18:25:47 +0200

    up, down, left, right

--- game.py
+++ game.py
@@ -10,8 +10,10 @@ class game:
         self.grid = np.zeros((RES_Y//50,RES_X//50))
         self.grid[0:3] = AIR
         self.grid[3,RES_X//100] = STUB
+        #self.grid[4][RES_X//100] = AIR
         self.roots = []
         self.roots.append(root.root(self,(3,RES_X//100),DOWN))
+        #self.roots[0].check_down()
 
     def run(self):
         self._if.run()
@@ -26,6 +28,7 @@ class game:
         self.grid = np.vstack([self.grid, np.zeros(RES_X//50)])
 
     def moveDown(self):
+        self.roots[0].check_down()
         self.scroll_up()
         self.grid_update()
         self._if.redrawGrid()
