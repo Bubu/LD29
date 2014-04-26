8e6c75f3a3ed720ea5040868839bf6ed96359df2
Marcus Hoffmann <bubuiic@aol.com>
2014-04-27 00:36:21 +0200

    splitting works somewhat

--- game.py
+++ game.py
@@ -12,7 +12,6 @@ class game:
         self.grid[3,RES_X//100][0] = SUP
         self.roots = []
         self.roots.append(root.root(self,(3,RES_X//100),DOWN))
-        #self.roots[0].check_down()
 
     def run(self):
         self._if.run()
@@ -21,7 +20,6 @@ class game:
         self._if.close()
 
     def scroll_up(self):
-        #self.grid = np.delete(self.grid, 0, 0)
         self.grid = np.roll(self.grid,-1,0)
         for r in self.roots:
             r.pos = (r.pos[0]-1,r.pos[1])
@@ -29,11 +27,10 @@ class game:
                 roots.remove(r)
 
     def grid_update(self):
-        #self.grid = np.vstack([self.grid, np.zeros(RES_X//50,dtype=(int,2))])
         self.grid[-1,:,0] = GROUND #TODO: Generate new, random tiles
 
     def move(self,check_dir):
-        lowest_ypos = 0
+        self.lowest_ypos = 0
         for root in self.roots:
             oldpos = root.pos
             olddir = root.dir
@@ -43,48 +40,9 @@ class game:
                 dir = root.check_right()
             elif check_dir == LEFT:
                 dir = root.check_left()
-                
             if dir is not None:
-                newpos = root.pos
-                if newpos[0] > lowest_ypos:
-                    lowest_ypos = newpos[0]
-                if dir == DOWN:
-                    self.grid[newpos][0] = SUP
-                    if olddir == DOWN:
-                        self.grid[oldpos][0] = UPDOWN
-                    elif olddir == RIGHT:
-                        self.grid[oldpos][0] = LEFTDOWN
-                    elif olddir == LEFT:
-                        self.grid[oldpos][0] = RIGHTDOWN
-                elif dir == UP:
-                    self.grid[newpos][0] = SDOWN
-                    if olddir == UP:
-                        self.grid[oldpos][0] = DOWNUP
-                    elif olddir == LEFT:
-                        self.grid[oldpos][0] = RIGHTUP
-                    elif olddir == RIGHT:
-                        self.grid[oldpos][0] = LEFTUP
-                elif dir == RIGHT:
-                    self.grid[newpos][0] = SLEFT
-                    if olddir == UP:
-                        self.grid[oldpos][0] = DOWNRIGHT
-                    elif olddir == DOWN:
-                        self.grid[oldpos][0] = UPRIGHT
-                    elif olddir == RIGHT:
-                        self.grid[oldpos][0] = LEFTRIGHT
-                elif dir == LEFT:
-                    self.grid[newpos][0] = SRIGHT
-                    if olddir == UP:
-                        self.grid[oldpos][0] = DOWNLEFT
-                    elif olddir == LEFT:
-                        self.grid[oldpos][0] = RIGHTLEFT
-                    elif olddir == DOWN:
-                        self.grid[oldpos][0] = UPLEFT
-                self._if.update([oldpos,newpos])
-        if lowest_ypos >= (RES_Y//50-5):
-            self.scroll_up()
-            self.grid_update()
-            self._if.redrawGrid()
+                self.updateSingleTile(root,dir,olddir,oldpos)
+        self.checkScroll()
 
     def triggerSplit(self):
         for r in self.roots:
@@ -93,6 +51,7 @@ class game:
             dir1,dir2 = r.check_split()
             if dir1 is not None:
                 if dir2 is not None:
+                    newpos1,newpos2 = self.getnewpos(oldpos,dir1,dir2)
                     if olddir == UP:
                         if dir1 == RIGHT:
                             if dir2 == LEFT:
@@ -130,12 +89,62 @@ class game:
                             if dir2 == DOWN:
                                 newtile = RIGHTLEFTDOWN
                     self.grid[oldpos][0] = newtile
+                    self._if.update([oldpos,newpos1,newpos2])
                 else:
-                    pass
-               
-        self._if.update([oldpos,newpos1,newpos2])
+                    self.updateSingleTile(r,dir1,olddir,oldpos)
         self.roots += self.newroots
-        
+
+    def updateSingleTile(self,root,dir,olddir,oldpos):
+        newpos = root.pos
+        if newpos[0] > self.lowest_ypos:
+            self.lowest_ypos = newpos[0]
+        if dir == DOWN:
+            self.grid[newpos][0] = SUP
+            if olddir == DOWN:
+                self.grid[oldpos][0] = UPDOWN
+            elif olddir == RIGHT:
+                self.grid[oldpos][0] = LEFTDOWN
+            elif olddir == LEFT:
+                self.grid[oldpos][0] = RIGHTDOWN
+        elif dir == UP:
+            self.grid[newpos][0] = SDOWN
+            if olddir == UP:
+                self.grid[oldpos][0] = DOWNUP
+            elif olddir == LEFT:
+                self.grid[oldpos][0] = RIGHTUP
+            elif olddir == RIGHT:
+                self.grid[oldpos][0] = LEFTUP
+        elif dir == RIGHT:
+            self.grid[newpos][0] = SLEFT
+            if olddir == UP:
+                self.grid[oldpos][0] = DOWNRIGHT
+            elif olddir == DOWN:
+                self.grid[oldpos][0] = UPRIGHT
+            elif olddir == RIGHT:
+                self.grid[oldpos][0] = LEFTRIGHT
+        elif dir == LEFT:
+            self.grid[newpos][0] = SRIGHT
+            if olddir == UP:
+                self.grid[oldpos][0] = DOWNLEFT
+            elif olddir == LEFT:
+                self.grid[oldpos][0] = RIGHTLEFT
+            elif olddir == DOWN:
+                self.grid[oldpos][0] = UPLEFT
+        self._if.update([oldpos,newpos])
+
+    def checkScroll(self):
+        if self.lowest_ypos >= (RES_Y//50-5):
+            self.scroll_up()
+            self.grid_update()
+            self._if.redrawGrid()
+            
+    def getnewpos(self,oldpos,dir1,dir2):
+        d = {DOWN:(1,0),UP:(-1,0),LEFT:(0,-1),RIGHT:(0,1)}
+        newpos1 = oldpos[0] + d[dir1][0],oldpos[1] + d[dir1][1]
+        newpos2 = oldpos[0] + d[dir2][0],oldpos[1] + d[dir2][1]
+        print(newpos1,newpos2)
+        return newpos1,newpos2
+    
 myGame = game()
 myGame.run()
 myGame.close()
