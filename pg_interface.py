6cfced14169b10842501e353c57a2f196afca0ef
Marcus Hoffmann <bubuiic@aol.com>
2014-04-26 15:42:51 +0200

    draw grid fix

--- pg_interface.py
+++ pg_interface.py
@@ -12,6 +12,7 @@ class interface:
         pg.init()
         self.screen = pg.display.set_mode((RES_X, RES_Y))
         pg.display.set_caption(GAME_NAME)
+        self.updateRects = []
         self.initSprites()
         
     def initSprites(self):
@@ -34,19 +35,30 @@ class interface:
             if evt.type == KEYUP and evt.key == K_F4 and bool(evt.mod & KMOD_ALT):
                 return
             else:
-                pass
+                if evt.type == K_DOWN:
+                    self.game.moveDown()
+                elif evt.type == K_DOWN:
+                    self.game.moveDown()
+                elif evt.type == K_RIGHT:
+                    self.game.moveRight()
+                elif evt.type == K_LEFT:
+                    self.game.moveLeft()
+                elif evt.type == K_SPACE:
+                    self.game.triggerSplit()
+
+                pygame.display.update(self.updateRects)
+                self.updateRects = []
 
     def setup(self):
         #self.screen.fill((196,195,192))
         self.drawGrid()
-        self.screen.blit(self.sprites[AIR],(50,50))
-        self.screen.blit(self.sprites[GROUND],(50,100))
 
     def drawGrid(self):
-        for coord in product(range(RES_X//50),range(RES_Y//50)):
+        for coord in product(range(RES_Y//50),range(RES_X//50)):
+            #print(coord)
             self.screen.blit(self.sprites[self.game.grid[coord]],self.getScreenPos(coord))
 
     def getScreenPos(self,coord):
-        x = ccord[0] * 50 + OFFSET_X
-        y = ccord[1] * 50 + OFFSET_Y
+        y = coord[0] * 50 + OFFSET_Y
+        x = coord[1] * 50 + OFFSET_X
         return x,y
